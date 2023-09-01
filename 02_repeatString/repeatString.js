01ca7cc579340cebab540229315bd3b35d096c46
Tom lewis <tom.lewis488@gmail.com>
2023-09-01 19:10:13 +0200

    Completed exercises 2, 3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) return 'ERROR'; 
 
+    let repeatedString = "";
+
+    while (num > 0) {
+        repeatedString += string;
+        num--;
+    }
+    return repeatedString;
 };
+repeatString('hey');
 
-// Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    return string.split('').reverse().join('')
 };
+reverseString('')
 
 // Do not edit below this line
 module.exports = reverseString;
