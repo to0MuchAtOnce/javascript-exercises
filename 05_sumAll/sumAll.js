4f0d96af09f7c4596c67538084093522e0e4d5e9
Tom lewis <tom.lewis488@gmail.com>
2023-09-04 17:36:27 +0200

    All tests passed in sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
 const sumAll = function(arg1, arg2) {
     let total = 0;
+    
+    if(arg1 < 0 || arg2 < 0) {
+        return 'ERROR';
+    } else if(typeof arg1 !== 'number' || typeof arg2 !== 'number') {
+        return "ERROR"
+    }
+    
+    if(arg1 > arg2) {
+        [arg1, arg2] = [arg2, arg1]
+    }
+
     for(let i = arg1; i <= arg2; i++) {
      total += i;
    } 
