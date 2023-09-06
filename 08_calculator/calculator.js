76f9234d6f64135fdcff97e3c3506d817a3b7b77
Tom lewis <tom.lewis488@gmail.com>
2023-09-06 18:51:00 +0200

    Updated calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,21 +6,29 @@ const subtract = function(num1, num2) {
   return num1 - num2;
 };
 
-const sum = function(num1, num2) {
+const sum = function(num) {
+  if (Array.isArray(num) && num.length === 0) {
+    return 0;
+  }  
+  else if (Array.isArray(num) && num.length === 1) { 
+    return num[0];
+  } else {
+    return 0;
+  };
 };
 
-const multiply = function(...nums) {
+const multiply = function(nums) {
   if (Array.isArray(nums)) {
     return nums[0] * nums[1];
   }
 }
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num1, num2) {
+  
 };
 
 // Do not edit below this line
