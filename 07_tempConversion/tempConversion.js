9ca6ad08e6d52015e814af826d79aae56b81e8b5
Tom Lewis <tom.lewis488@gmail.com>
2023-09-06 10:45:36 +0200

    Updated tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,10 @@
 const convertToCelsius = function(far) {
-  const cel = Math.round((far - 32) / 1.8 * 10) / 10; 
+  const cel = Math.round((far - 32) * (5 / 9) * 10) / 10; 
   return cel;
 };
 
 const convertToFahrenheit = function(cel) {
-const far = Math.round(((cel * 1.8) + 32) * 10) / 10;
+const far = Math.round(((cel * 9) / 5 + 32) * 10) / 10;
 return far;
 };
 
