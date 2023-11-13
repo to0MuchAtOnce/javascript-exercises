4fff72f689314ca6354122f1886bdbe6e0f642fc
Tom Lewis <tom.lewis488@gmail.com>
2023-11-13 16:39:57 +0100

    Update 09 - 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  if (num < 0) return 'OOPS';
 
+  let prevNum = 1;
+  let secondPrevNum = 0;
+
+  for (let i = 1; i < num; i++) {
+    let totalNum = prevNum + secondPrevNum;
+    secondPrevNum = prevNum;
+    prevNum = totalNum;
+  }
+  return prevNum;
 };
 
 // Do not edit below this line
