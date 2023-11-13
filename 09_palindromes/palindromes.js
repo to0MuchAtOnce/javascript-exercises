5a53d33d79a512212d010f35d16fd78c0fedd683
Tom Lewis <tom.lewis488@gmail.com>
2023-11-13 16:42:29 +0100

    Update 09 - 10

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  // Remove non-alphanumeric characters and convert to lowercase
+  let cleanStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+  return cleanStr.split('').reverse().join('') === cleanStr;
 };
 
 // Do not edit below this line
