3a7101b402aac1db7ae67f5e9a9d550919b34d64
Tom Lewis <tom.lewis488@gmail.com>
2023-11-13 17:38:56 +0100

    Complete 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+  let result = books.map((books) => books.title);
+  return result;
 };
 
 // Do not edit below this line
