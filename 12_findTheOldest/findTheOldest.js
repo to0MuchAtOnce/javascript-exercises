7209cb3ed07d2e9ef4386d53a4e52790ebc50e42
Tom lewis <tom.lewis488@gmail.com>
2023-11-26 14:51:06 +0100

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+  // Reduce the array by comparing the current age with the previous age.
+  return array.reduce((oldest, currentPerson) => {
+    //
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    console.log(oldestAge);
+    const currentAge = getAge(
+      currentPerson.yearOfBirth,
+      currentPerson.yearOfDeath
+    );
+    return oldestAge < currentAge ? currentPerson : oldest;
+  });
+};
 
+const getAge = function (birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
 };
 
 // Do not edit below this line
